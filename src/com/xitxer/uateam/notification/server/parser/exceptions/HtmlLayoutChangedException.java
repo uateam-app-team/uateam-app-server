9e63ad967a12c98cddfce4efda8f2044da235103
voidset <zipu4.post@gmail.com>
2012-07-24 12:07:51 +0300

    add maven pom.xml stub remove code that will be as separate project

--- src/com/xitxer/uateam/notification/server/parser/exceptions/HtmlLayoutChangedException.java
+++
@@ -1,9 +0,0 @@
-package com.xitxer.uateam.notification.server.parser.exceptions;
-
-public class HtmlLayoutChangedException extends Exception {
-	private static final long serialVersionUID = -8237394394164949344L;
-
-	public HtmlLayoutChangedException(String string) {
-		super(string);
-	}
-}
